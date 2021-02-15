cf2ae0fa7e54bfa95c13e627f9f04ee14b5cc776
hwanyyy <goooogler94@gmail.com>
2021-02-16 06:32:23 +0900

    [update] test -> detect

---
+++ result/detect_result.sh
@@ -0,0 +1,1 @@
+python detect_result.py --weights yolov5x_best.pt --source ../dataset/images/val/ --save-conf --save-txt --iou-thres 0.5 --img-size 640
--- result/test.sh
+++
@@ -1,1 +0,0 @@
-python test_result.py --batch-size 63 --weights yolov5x_best.pt --data yolo.yaml --iou-thres 0.5  --img 640 --save-txt --save-conf
